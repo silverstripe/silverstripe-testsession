81d39c23504f5a74c417b36c35b6b924d904508f
Ingo Schommer <ingo@silverstripe.com>
2013-11-27 19:29:44 +0100

    NEW Write PHP code for later inclusion
    Useful for out-of-process stubs with Behat+Selenium

---
+++ tests/unit/TestSessionStubCodeWriterTest.php
@@ -0,0 +1,43 @@
+<?php
+class TestSessionStubCodeWriterTest extends SapphireTest {
+
+	public function tearDown() {
+		parent::tearDown();
+
+		$file = TEMP_FOLDER . '/TestSessionStubCodeWriterTest-file.php';
+		if(file_exists($file)) unlink($file);
+	}
+
+	public function testWritesHeaderOnNewFile() {
+		$file = TEMP_FOLDER . '/TestSessionStubCodeWriterTest-file.php';
+		$writer = new TestSessionStubCodeWriter($file);
+		$writer->write('foo();', false);
+		$this->assertFileExists($file);
+		$this->assertEquals(
+			file_get_contents($writer->getFilePath()),
+			"<?php\nfoo();\n"
+		);
+	}
+
+	public function testWritesWithAppendOnExistingFile() {
+		$file = TEMP_FOLDER . '/TestSessionStubCodeWriterTest-file.php';
+		$writer = new TestSessionStubCodeWriter($file);
+		$writer->write('foo();', false);
+		$writer->write('bar();', false);
+		$this->assertFileExists($file);
+		$this->assertEquals(
+			file_get_contents($writer->getFilePath()),
+			"<?php\nfoo();\nbar();\n"
+		);
+	}
+
+	public function testReset() {
+		$file = TEMP_FOLDER . '/TestSessionStubCodeWriterTest-file.php';
+		$writer = new TestSessionStubCodeWriter($file);
+		$writer->write('foo();', false);
+		$this->assertFileExists($file);
+		$writer->reset();
+		$this->assertFileNotExists($file);
+	}
+
+}
